78dffa2251ef09becf73247c2bc3345b8119a156
tbev6617 <31513877+tbev6617@users.noreply.github.com>
2017-09-15 09:02:40 -0600

    Created my getters and setters

--- src/monster/controller/MonsterController.java
+++ src/monster/controller/MonsterController.java
@@ -6,8 +6,12 @@ public class MonsterController
 {
 	public void start() 
 	{
-		MarshmallowMonster myMonster = new MarshmallowMonster();
-		System.out.println(myMonster);
+		/*
+		MarshmallowMonster sample = new MarshmallowMonster();
+		System.out.println(sample);
+		*/
+		
+		MarshmallowMonster Albert = new MarshmallowMonster("Albert", 6, 6, 0, true);
+		System.out.println(Albert);
 	}
-
 }
--- src/monster/model/MarshmallowMonster.java
+++ src/monster/model/MarshmallowMonster.java
@@ -19,9 +19,53 @@ public class MarshmallowMonster
 		this.tentacleAmount = tentacleAmount;
 		this.hasBloop = hasBloop;
 	}
+	//GETTERS
+	public String getName() 
+	{
+		return name;
+	}
+	public int getEyeCount() 
+	{
+		return eyeCount;
+	}
+	public int getArmCount() 
+	{
+		return armCount;
+	}
+	public Double getTentacleAmount()
+	{
+		return tentacleAmount;
+	}
+	public boolean hasBloop()
+	{
+		return hasBloop;
+	}
+	
+	//SETTERS
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+	public void setEyeCount(int eyeCount)
+	{
+		this.eyeCount = eyeCount;
+	}
+	public void setArmCount(int armCount)
+	{
+		this.armCount = armCount;
+	}
+	public void setTentacleAmount(Double tentacleAmount)
+	{
+		this.tentacleAmount = tentacleAmount;
+	}
+	public void setHasBloop(boolean hasBloop)
+	{
+		this.hasBloop = hasBloop;
+	}
+	
 	public String toString()
 	{
-		String description = "Hi I am a monster named " + name + "! I have " + eyeCount + " eyes and " + armCount + " arms.";
+		String description = "Hi I am a monster named " + name + "! I have " + eyeCount + " eyes and " + armCount + " arms. ";
 		description += "I also have "+ tentacleAmount + " tentacles and my bloop existence is " + hasBloop;
 		return description;
 	}
